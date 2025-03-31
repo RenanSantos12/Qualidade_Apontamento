b05f1f910c1c86780b42a59dc1fbd4489efd606c
RenanSantos12 <renansantos140406@gmail.com>
2025-03-31 14:53:11 -0300

    arquivo teste.py, para rodar manual caso precise

--- teste.py
+++ teste.py
@@ -1,18 +1,22 @@
 from playwright.sync_api import sync_playwright
 from datetime import datetime
 from time import sleep
-from time import sleep
 
 
 class Apontamento:
 
         def __init__(self):
-                self.dia_atual = datetime.now().strftime('%d').lstrip('0')
-                self.dia_mes_atual = datetime.now().strftime('%m')
-                self.mes_ = datetime.today().strftime('%B')
+
+                # self.dia_atual = datetime.now().strftime('%d').lstrip('0')
+                # self.dia_mes_atual = datetime.now().strftime('%m')
+                # self.mes_ = datetime.today().strftime('%B')
+                # print(self.dia_atual)
+                # print(self.mes_)
+                # March
+                self.dia_atual = int(28)
+                self.mes_ = 'March'
                 print(self.dia_atual)
                 print(self.mes_)
-
         def site(self):
                 with sync_playwright() as playwright_:
                         browser_ = playwright_.chromium.launch(headless=False)
@@ -22,28 +26,29 @@ class Apontamento:
                         page.goto("https://forms.clickup.com/9013267565/f/8ckq33d-6313/LIRPXERWM7K3PMYXWV")
                         sleep(5)
                         page.locator("[data-test=\"form__body-item__RE\"] [data-test=\"select__dropdown__toggle\"]").click()
-                        sleep(1)
+                        sleep(0.5)
                         page.locator("[data-test=\"select__search-field\"]").fill("Renan Santos")
-                        sleep(1)
-                        page.get_by_role("option", name="- RENAN SANTOS ARAUJO").locator("div").nth(1).click()
-                        sleep(1)
+                        sleep(0.5)
+                        page.get_by_role("option", name="- RENAN SANTOS ARAUJO").locator("div").nth(0.5).click()
+                        sleep(0.5)
                         page.locator(
                             "[data-test=\"form__body-item__Cliente - Projeto\"] [data-test=\"select__dropdown__toggle\"]").click()
-                        sleep(1)
+                        sleep(0.5)
                         page.locator("[data-test=\"select__search-field\"]").fill("QU")
-                        sleep(1)
-                        page.get_by_role("option", name="Paranoá|Produtos - RPA").locator("div").nth(1).click()
-                        sleep(1)
-                        page.locator("[data-test=\"form__date-picker-input-start-date\"]").click()
-                        sleep(1)
+                        sleep(0.5)
+                        page.get_by_role("option", name="Paranoá|Produtos - RPA").locator("div").nth(0.5).click()
+                        sleep(0.5)
+                        page.locator("[data-test=\"form__date-picker-input-start-date\"]").click(force=True)
+                        sleep(0.5)
                         page.get_by_label(f"{self.mes_} {self.dia_atual},").first.click()
-                        sleep(1)
+                        sleep(0.5)
                         page.get_by_role("spinbutton", name="Hour").fill("08")
-                        sleep(1)
+                        sleep(0.5)
                         check_manha =  '/html/body/div[1]/div[3]/span[2]'
                         if not check_manha:
                                 page.get_by_text("PM").nth(1).click()
                         sleep(1)
+                        # page.get_by_role("button", name="Selecione a data de vencimento").click()
                         page.get_by_role("button", name="Select Due Date").click()
                         sleep(1)
                         page.get_by_label(f"{self.mes_} {self.dia_atual},").nth(1).click()
@@ -55,13 +60,10 @@ class Apontamento:
                         page.locator('xpath=//*[@id="cu-form-control-5"]').click()
                         sleep(1)
                         page.locator("[data-test=\"form__submit-btn\"]").click()
-                        print(3)
+                        sleep(3)
                 print('concluido')
 
 if __name__ == "__main__":
+
         execute = Apontamento()
         execute.site()
-
-
-
-
