ee512cfaa63404cff8daa892a26f7a04a869fd1d
RenanSantos12 <renansantos140406@gmail.com>
2025-07-18 09:02:01 -0300

    Aponta dias anteriores

--- teste.py
+++ teste.py
@@ -5,37 +5,39 @@ import random
 
 
 class Apontamento:
+
     def __init__(self):
         # self.dia_atual = datetime.now().strftime('%d').lstrip('0')
         # self.dia_mes_atual = datetime.now().strftime('%m')
         # self.mes_ = datetime.today().strftime('%B')
-        self.dia_atual = int(16)
-        self.mes_ = 'June'
+        self.dia_atual = int(15)
+        self.mes_ = 'July'
         self.trabalho = ['Fiscal faturamento', 'RPA Qualidade', 'RPA CND']
         print(self.dia_atual)
         print(self.mes_)
 
     def parte1(self, page):
-        page.goto("https://forms.clickup.com/9013267565/f/8ckq33d-6673/48PU0U96L8AJ2EFQQJ")
+        page.goto("https://forms.clickup.com/9013267565/f/8ckq33d-9313/K13NOYVFNI70YT4ALA")
         sleep(5)
         page.locator("[data-test=\"form__body-item__RE\"] [data-test=\"select__dropdown__toggle\"]").click()
-        sleep(0.5)
+        sleep(1)
         page.locator("[data-test=\"select__search-field\"]").fill("Renan Santos")
-        sleep(0.5)
+        sleep(1)
         page.get_by_role("option", name="- RENAN SANTOS ARAUJO").locator("div").nth(1.5).click()
-        sleep(0.5)
+        sleep(1)
         page.locator(
             "[data-test=\"form__body-item__Cliente - Projeto\"] [data-test=\"select__dropdown__toggle\"]").click()
-        sleep(0.5)
+        sleep(1)
         projeto = random.choice(self.trabalho)
         print(projeto)
         page.locator("[data-test=\"select__search-field\"]").fill(projeto)
-        sleep(0.5)
+        sleep(1)
         page.get_by_role("option", name=projeto).locator("div").nth(1.5).click()
-        sleep(0.5)
-        page.locator("[data-test=\"form__date-picker-input-start-date\"]").click(force=True)
-        sleep(0.5)
-        page.get_by_label(f"{self.mes_} {self.dia_atual},").first.click()
+        sleep(1)
+        page.locator("[data-test=\"form__date-picker-input-start-date\"]").click()
+        sleep(1)
+        page.locator("[data-test=\"form__body-item__Start date\"]").get_by_label(
+            f"{self.mes_} {self.dia_atual},").click()
         sleep(1)
         page.get_by_role("spinbutton", name="Hour").fill("08")
         sleep(1)
@@ -44,7 +46,8 @@ class Apontamento:
         if not check_manha:
             print(2)
             page.get_by_text("PM").nth(1).click()
-        sleep(0.5)
+        sleep(1)
+        print(3)
         page.get_by_role("button", name="Select Due Date").click()
         sleep(1)
         page.get_by_label(f"{self.mes_} {self.dia_atual},").nth(1).click()
@@ -53,15 +56,13 @@ class Apontamento:
         sleep(1)
         page.get_by_role("spinbutton", name="Hour").fill("12")
         sleep(2)
-        page.locator('xpath=//*[@id="cu-form-control-5"]').click()
-        sleep(3)
+        page.get_by_role("textbox", name="Unidade de Produção").click()
+        sleep(1)
         page.locator("[data-test=\"form__submit-btn\"]").click()
         sleep(5)
         print('concluido')
-
-
     def parte2(self, page):
-        page.goto("https://forms.clickup.com/9013267565/f/8ckq33d-6673/48PU0U96L8AJ2EFQQJ")
+        page.goto("https://forms.clickup.com/9013267565/f/8ckq33d-9313/K13NOYVFNI70YT4ALA")
         sleep(5)
         page.locator("[data-test=\"form__body-item__RE\"] [data-test=\"select__dropdown__toggle\"]").click()
         sleep(1)
@@ -80,30 +81,24 @@ class Apontamento:
         sleep(1)
         page.locator("[data-test=\"form__date-picker-input-start-date\"]").click(force=True)
         sleep(1)
-        page.get_by_label(f"{self.mes_} {self.dia_atual},").first.click()
+        page.locator("[data-test=\"form__body-item__Start date\"]").get_by_label(
+            f"{self.mes_} {self.dia_atual},").click()
         sleep(1)
-        page.get_by_role("spinbutton", name="Hour").fill("13")
+        page.locator("[data-test=\"form__body-item__Start date\"]").get_by_text("AM").nth(1).click()
         sleep(1)
-        print(1)
-        check_manha = '/html/body/div[1]/div[3]/span[2]'
-        if not check_manha:
-            print(2)
-            page.get_by_text("PM").nth(1).click()
+        page.get_by_role("spinbutton", name="Hour").fill("01")
         sleep(1)
         page.get_by_role("button", name="Select Due Date").click()
         sleep(1)
         page.get_by_label(f"{self.mes_} {self.dia_atual},").nth(1).click()
-        if '/html/body/div[2]/div[3]/span[2]':
-            page.get_by_text("AM").nth(4).click()
-        sleep(1)
+        sleep(0.5)
         page.get_by_role("spinbutton", name="Hour").fill("03")
         sleep(2)
-        page.locator('xpath=//*[@id="cu-form-control-5"]').click()
+        page.get_by_role("textbox", name="Unidade de Produção").click()
         sleep(1)
-        # page.locator("[data-test=\"form__submit-btn\"]").click()
+        page.locator("[data-test=\"form__submit-btn\"]").click()
         sleep(3)
         print('concluido')
-        browser_.close()
 
 if __name__ == "__main__":
     with sync_playwright() as playwright_:
@@ -114,7 +109,6 @@ if __name__ == "__main__":
         execute = Apontamento()
         execute.parte1(page_)
         execute.parte2(page_)
-
 
 
 
