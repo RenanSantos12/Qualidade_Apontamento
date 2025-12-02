d493202841cfd7400d006ce69d29212290dd9761
RenanSantos12 <renansantos140406@gmail.com>
2025-12-02 15:11:05 -0300

    Atualizado o teste para apontar em massa

--- teste.py
+++ teste.py
@@ -6,73 +6,62 @@ import random
 class Apontamento:
 
     def __init__(self):
-        # self.dia_atual = datetime.now().strftime('%d').lstrip('0')
-        # self.dia_mes_atual = datetime.now().strftime('%m')
-        # self.mes_ = datetime.today().strftime('%B')
-        self.dia_atual = int(10)
-        self.mes_ = 'October'
+        self.dia_atual = 3, 4
+        self.mes_ = 'November'
         self.trabalho = ['Fiscal faturamento', 'RPA Qualidade', 'RPA CND']
-        print(self.dia_atual)
-        print(self.mes_)
 
-    def parte1(self, page):
-        page.goto("https://forms.clickup.com/9013267565/f/8ckq33d-10313/DSAPWIX3VRL5AU5NL2")
-        sleep(5)
-        page.locator("[data-test=\"form__body-item__RE\"] [data-test=\"select__dropdown__toggle\"]").click()
-        sleep(1)
-        page.locator("[data-test=\"select__search-field\"]").fill("Renan Santos")
-        sleep(1)
-        page.get_by_role("option", name="- RENAN SANTOS ARAUJO").locator("div").nth(1.5).click()
-        sleep(1)
-        page.locator(
-            "[data-test=\"form__body-item__Cliente - Projeto\"] [data-test=\"select__dropdown__toggle\"]").click()
-        sleep(1)
-        projeto = random.choice(self.trabalho)
-        print(projeto)
-        page.locator("[data-test=\"select__search-field\"]").fill(projeto)
-        sleep(1)
-        page.get_by_role("option", name=projeto).locator("div").nth(1.5).click()
-        sleep(1)
-        page.locator("[data-test=\"form__date-picker-input-start-date\"]").click()
-        sleep(1)
-        page.locator("[data-test=\"form__body-item__Start date\"]").get_by_label(
-            f"{self.mes_} {self.dia_atual},").click()
-        sleep(1)
-        page.get_by_role("spinbutton", name="Hour").fill("07")
-        sleep(0.5)
-        page.get_by_role("spinbutton", name="Minute").click()
-        sleep(0.5)
-        page.get_by_role("spinbutton", name="Minute").fill("30")
-        sleep(0.5)
-        page.get_by_role("spinbutton", name="Minute").press("Enter")
-        sleep(1)
-        #page.get_by_role("button", name="Select Due Date").click()
-        #sleep(0.5)
-        #page.locator("[data-test=\"form__body-item__Due date\"]").get_by_text("AM").click()
-        #check_manha = '/html/body/div[1]/div[3]/span[2]'
-        #if not check_manha:
-        #    print(2)
-        #    page.get_by_text("PM").nth(1).click()
-        sleep(1)
-        print(3)
-        page.get_by_role("button", name="Select Due Date").click()
-        sleep(0.5)
-        page.locator("[data-test=\"form__body-item__Due date\"]").get_by_text("AM").click()
-        sleep(0.5)
-        #page.locator("[data-test=\"form__body-item__Due date\"]").get_by_label(f"{self.mes_} {self.dia_atual},").click()
-        page.get_by_label(f"{self.mes_} {self.dia_atual},").nth(1).click()
-        sleep(0.5)
-        page.get_by_role("spinbutton", name="Hour").click()
-        page.get_by_role("spinbutton", name="Hour").press("ArrowRight")
-        sleep(0.5)
-        page.get_by_role("spinbutton", name="Hour").fill("16")
-        page.get_by_role("spinbutton", name="Minute").fill("00")
-        sleep(0.5)
-        page.get_by_role("spinbutton", name="Hour").press("Enter")
-        page.get_by_role("textbox", name="Unidade de Produção").click()
-        sleep(1)
-        page.locator("[data-test=\"form__submit-btn\"]").click()
-        sleep(3)
+    def parte1(self, page, dia_atual):
+        try:
+            page.goto("https://forms.clickup.com/9013267565/f/8ckq33d-11993/5GCZKRV3FZPKLCXY1Q")
+            sleep(5)
+            page.locator("[data-test=\"form__body-item__RE\"] [data-test=\"select__dropdown__toggle\"]").click()
+            sleep(1)
+            page.locator("[data-test=\"select__search-field\"]").fill("Renan Santos")
+            sleep(1)
+            page.get_by_role("option", name="- RENAN SANTOS ARAUJO").locator("div").nth(1.5).click()
+            sleep(1)
+            page.locator(
+                "[data-test=\"form__body-item__Cliente - Projeto\"] [data-test=\"select__dropdown__toggle\"]").click()
+            sleep(1)
+            projeto = random.choice(self.trabalho)
+            print(projeto)
+            page.locator("[data-test=\"select__search-field\"]").fill(projeto)
+            sleep(1)
+            page.get_by_role("option", name=projeto).locator("div").nth(1.5).click()
+            sleep(1)
+            page.locator("[data-test=\"form__date-picker-input-start-date\"]").click()
+            sleep(1)
+            page.locator("[data-test=\"form__body-item__Start date\"]").get_by_label(
+                f"{self.mes_} {dia_atual},").click()
+            sleep(1)
+            page.get_by_role("spinbutton", name="Hour").fill("07")
+            sleep(0.5)
+            page.get_by_role("spinbutton", name="Minute").click()
+            sleep(0.5)
+            page.get_by_role("spinbutton", name="Minute").fill("30")
+            sleep(0.5)
+            page.get_by_role("spinbutton", name="Minute").press("Enter")
+            sleep(2)
+            page.get_by_role("button", name="Select Due Date").click()
+            sleep(0.5)
+            page.locator("[data-test=\"form__body-item__Due date\"]").get_by_text("AM").click()
+            sleep(0.5)
+            #page.locator("[data-test=\"form__body-item__Due date\"]").get_by_label(f"{self.mes_} {self.dia_atual},").click()
+            page.get_by_label(f"{self.mes_} {dia_atual},").nth(1).click()
+            sleep(0.5)
+            page.get_by_role("spinbutton", name="Hour").click()
+            page.get_by_role("spinbutton", name="Hour").press("ArrowRight")
+            sleep(0.5)
+            page.get_by_role("spinbutton", name="Hour").fill("16")
+            page.get_by_role("spinbutton", name="Minute").fill("00")
+            sleep(0.5)
+            page.get_by_role("spinbutton", name="Hour").press("Enter")
+            page.get_by_role("textbox", name="Unidade de Produção").click()
+            sleep(1)
+            page.locator("[data-test=\"form__submit-btn\"]").click()
+            sleep(3)
+        except Exception as erro_site:
+            print('Erro ao tentar apertar apontar no site', erro_site)
 
 
     def parte2(self, page):
@@ -114,6 +103,14 @@ class Apontamento:
         sleep(3)
         print('concluido')
 
+
+    def run(self, page):
+        dias = self.dia_atual
+        for dia in dias:
+            self.parte1(page, dia)
+
+
+
 if __name__ == "__main__":
     with sync_playwright() as playwright_:
         browser_ = playwright_.chromium.launch(headless=False)
@@ -121,7 +118,7 @@ if __name__ == "__main__":
         page_ = context_.new_page()
 
         execute = Apontamento()
-        execute.parte1(page_)
+        execute.run(page_)
         # execute.parte2(page_)
 
 
